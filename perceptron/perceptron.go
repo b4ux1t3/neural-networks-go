e90fed042b55a5bbb3e7a203f16b7aaa6cc3ca3f
Chris Pilcher <christopher.a.pilcher@gmail.com>
2017-06-27 01:17:40 -0400

    Perceptron ready to be tested

---
+++ perceptron/perceptron.go
@@ -0,0 +1,50 @@
+package perceptron
+
+// This is a Go implementation of the perceptron as created by Daniel Shiffman of The Coding Train in this video:
+// https://youtu.be/ntKn5TPHHAk
+
+import (
+	"math/rand"
+
+	"github.com/google/gofuzz"
+)
+
+var weights []float64
+
+// Starts up the Perceptron, initializing the two input weights, and checking if we are in debug mode
+func setup() {
+	f := fuzz.New().NilChance(0)
+	// Generate the weights
+	weights = generateWeights(*f)
+}
+
+// Guess returns +1 or -1 based on the sum of its inputs times those inputs' weights.
+func Guess(inputs []float64) int {
+	var sum float64
+
+	setup()
+
+	for i := range inputs {
+		sum += inputs[i] * weights[i]
+	}
+
+	if sum >= 0 {
+		return 1
+	}
+	return -1
+}
+
+// Simply fills out the weights of the Perceptron, giving them a random value between -1 and 1
+func generateWeights(f fuzz.Fuzzer) []float64 {
+	output := make([]float64, 2)
+
+	// Generate random weights
+	for i := range output {
+		f.Fuzz(&output[i])
+		// Half the time, flip the sign
+		if rand.Float64() > 0.5 {
+			output[i] *= -1
+		}
+	}
+	return output
+}
